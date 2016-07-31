3ddba7e9c10d58cdff083f78fe79df75f6d38ceb
speedDeveloper <info@speeddeveloper.me>
2016-08-01 00:16:37 +0200

    Update app.js, database.js

--- app/app.js
+++ app/app.js
@@ -6,8 +6,76 @@ var database = require("./database.js");
 
 
 
-database.createHub("TheHubName", 2);
 
-var x = 0;
+
+
+
+
+
+
+
+
+// Test Database stuff
+
+var id = database.createHub("TheHubName", 2);
+/*database.getHubById(id).then(function (hub) {
+    console.log("Hub found - getHubById()");
+    console.log(hub.id);
+    console.log(hub.name);
+    console.log(hub.ownerUid);
+}, function () {
+    console.log("error retriving Hub - getHubById()");
+});*/
+
+
+database.getMyHubs(2).then(function (hubArray) {
+    console.log("Hub found - getHubById()");
+    hubArray.forEach(function (hub) {
+        console.log(hub.id);
+        console.log(hub.name);
+        console.log(hub.ownerUid);
+    });
+}, function () {
+    console.log("error retriving Hub - getHubById()");
+});
+database.deleteHubById("-KO0MT9YYCjFtbCtryh6", 2);
+
+
+/*
+var id = database.createHub("TheHubName", 2);
+console.log(id);
+//database.deleteHubById(id, 2);
+
+
+database.getHubById(id, function (hub) {
+    console.log(hub.id);
+    console.log(hub.name);
+    console.log(hub.ownerUid);
+}, function () {
+    console.log("does not exist");
+});
+
+
+database.getMyHubs(2, function (hubIds) {
+    console.log(hubIds);
+}, function () {
+    
+});
+*/
+
+/*
+
+database.createHub("NewHub", 1);
+
+
+database.getHubById("-KO01LzgKspiWbHa_oUo", function (hub) {
+    console.log(hub.id);
+    console.log(hub.name);
+    console.log(hub.ownerUid);
+}, function () {
+    console.log("does not exist");
+});
+*/
+
 
 
--- app/database.js
+++ app/database.js
@@ -1,8 +1,11 @@
-/*global require, module */
+/*global require, module, Promise */
 /*jslint node: true */
+/*jshint es5: false */
+/*jshint esnext: true */
 "use strict";
-
-
+var KEY_HUB = "hub";
+var KEY_USER = "user";
+var NODE_SEP = "/";
 var firebase = require("firebase");
 var exports = module.exports = {};
 // Initialize Firebase
@@ -13,51 +16,72 @@ var config = {
     storageBucket: "project-mango-5d7d3.appspot.com"
 };
 firebase.initializeApp(config);
-
+var database = firebase.database();
 // ------------------- Hub Methods --------------------------- 
-
-exports.createHub = function (name, ownerUserId) {
-    firebase.database().ref('hubs/' + name).set({
-        "ownerUserId": ownerUserId
+exports.createHub = function (name, ownerUid) {
+    var reference = database.ref(KEY_HUB + NODE_SEP).push({
+        "name": name,
+        "ownerUid": ownerUid
     });
+    database.ref(KEY_USER + NODE_SEP + ownerUid + NODE_SEP + KEY_HUB + NODE_SEP + reference.key).set(true);
+    return reference.key;
 };
-
-exports.checkIfHubExists = function (name) {
-    
-    
+exports.deleteHubById = function (id, ownerUid) {
+    database.ref(KEY_HUB + NODE_SEP + id).remove();
+    database.ref(KEY_USER + NODE_SEP + ownerUid + NODE_SEP + KEY_HUB + NODE_SEP + id).remove();
 };
-
-
-exports.deleteHub = function () {
-    
-    
+exports.getHubById = function (hubId) {
+    return new Promise(
+        function (resolve, reject) {
+            database.ref(KEY_HUB + NODE_SEP + hubId).once('value').then(function (snapshot) {
+                if (snapshot === null || snapshot.val() === null) {
+                    reject();
+                } else {
+                    var o = snapshot.val();
+                    o.id = snapshot.key;
+                    resolve(o);
+                }
+                
+            });
+        }
+    );
 };
-
-exports.getHubById = function () {
-    
-    
+exports.getMyHubIds = function (ownerUid) {
+    return new Promise(
+        function (resolve, reject) {
+            database.ref(KEY_USER + NODE_SEP + ownerUid + NODE_SEP + KEY_HUB).once('value').then(function (snapshot) {
+                if (snapshot === null || snapshot.val() === null) {
+                    reject();
+                } else {
+                    resolve(Object.keys(snapshot.val()));
+                }
+            });
+        }
+    );
 };
-
-exports.getMyHub = function () {
-    
-    
+exports.getMyHubs = function (ownerUid) {
+    return new Promise(
+        function (resolve, reject) {
+            exports.getMyHubIds(ownerUid).then(function (hubIds) {
+                
+                var promises = [];
+                
+                hubIds.forEach(function (value) {
+                    promises.push(exports.getHubById(value));
+                });
+                
+                Promise.all(promises).then(function (value) {
+                    resolve(value);
+                }, function () {
+                    reject();
+                });
+            }, function () {
+                reject();
+            });
+        }
+    );
 };
-
-
-exports.editHub = function () {
-    
-    
-};
-
-
+exports.editHub = function () {};
 // ------------------- File Methods --------------------------- 
-
-exports.addFileToHub = function () {
-    
-    
-};
-
-exports.removeFileFromHub = function () {
-    
-    
-};
+exports.addFileToHub = function () {};
+exports.removeFileFromHub = function () {};

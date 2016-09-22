0d7fa21d11e81964cd64d340088427c77e2c0ed3
Bailey Griswold <baymose@gmail.com>
2016-09-21 21:32:37 -0500

    Adds create user, logging in, signing out, etc.

--- www/js/angularApp.js
+++ www/js/angularApp.js
@@ -1,16 +1,25 @@
+Baileys-MacBook-Pro:fridge2.0 bay$ clear
 var app = angular.module('mango',[]);
 
-app.controller('RegisterCtrl', [
+app.controller('AuthCtrl', [
         '$scope',
         function($scope) {
             $scope.loginUser = function() {
-				firebase.auth().signInWithEmailAndPassword($scope.email, $scope.password).catch(function(error) {
-				  // Handle Errors here.
-				  if(error.code) {
-					console.log(error.code);
-					console.log(error.message);
-					}
-				});
+                firebase.auth().signInWithEmailAndPassword($scope.email, $scope.password).catch(function(error) {
+                    // Handle Errors here.
+                    if(error.code) {
+                        console.log(error.code);
+                        console.log(error.message);
+                    }
+                });
+
+            };
+            $scope.signOutUser = function() {
+                firebase.auth().signOut().then(function() {
+                      console.log('Signed Out');
+                }, function(error) {
+                      console.error('Sign Out Error', error);
+                });
 
             };
 
@@ -21,15 +30,30 @@ app.controller('RegisterCtrl', [
                         var errorMessage = error.message;
                         console.log(errorMessage);
                         console.log(errorCode);
+                        console.log("error!!!");
+                        return;
                     }
-                    else {
-						var user = firebase.auth().currentUser;
-						if (user) {
-						}
-						else {
-						}
-					}
+                });
+                var user = firebase.auth().currentUser;
+                console.log("User Created");
+                firebase.database().ref('users/' + user.uid).set({
+                    uuid: user.uid,
+                    username: $scope.username,
                 });
 
             };
         }]);
+
+app.controller('HubCtrl', [
+    '$scope',
+    function($scope) {
+        $scope.createHub = function() {
+        };
+    }]);
+app.controller('HomeCtrl', [
+    '$scope',
+    function($scope) {
+        $scope.createHub = function() {
+        $scope.username = firebase.auth().currentUser.uid;
+        };
+    }]);

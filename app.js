6d10384938230a7d29f9fc21c0c20564851cfed5
speedDeveloper <info@speeddeveloper.me>
2016-09-24 13:25:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var server = app.listen('80', function () {
   console.log('Press Ctrl+C to quit.');
 });
 
-app.use(express.static(__dirname + '/www/'));
+//app.use(express.static(__dirname + '/www/'));
 
 app.get('/', function (req, res) {
   res.status(200).sendFile(path.join(__dirname + '/www/html/welcome.html'));

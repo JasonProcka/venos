1e85798a7ef37215599519ee3b465119263c72f6
speedDeveloper <info@speeddeveloper.me>
2016-10-22 20:01:23 +0200

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Nav from '../containers/Nav';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
+        <Nav />
         {this.props.children}
       </div>
     );

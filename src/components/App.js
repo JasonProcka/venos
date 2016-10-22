baa218520191fdc4db082d13ed491678e92fed74
speedDeveloper <info@speeddeveloper.me>
2016-10-23 00:03:01 +0200

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Nav from '../containers/Nav';
-
+import { Link } from 'react-router';
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <Nav />
         {this.props.children}
+        <Link to="/signin">Link</Link>
       </div>
     );
   }

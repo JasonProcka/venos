0bb52a98d4d3abb2330665d0a0329b1a41709f3a
jason <derproka@gmail.com>
2016-11-21 01:46:55 +0000

    update c9

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import '../styles/home.css';
+import '../styles/grid.css';
+
 
 class Home extends React.Component {
+   constructor(props) {
+     super(props);
+   }
   render() {
-    return <div>Home</div>;
+  return (
+  <div className="home">
+  <div className="introWrap">
+    <div className="introTop">
+      <div className="introContent">
+        <h3>Venos</h3>
+        <p>Store valuable resources in a collaborative location</p>
+        <div className="introButtons">
+          <FlatButton className="learn" label="Learn More" />
+          <RaisedButton label="Create Hub" secondary={`true`} />
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  );
   }
 }
 
